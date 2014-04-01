e88084d705bfc96c070d1e484b515941b7b0ba9e
omegahsieh <slohsieh@gmail.com>
2014-04-01 17:46:56 +0800

    milk2

--- section 1/1.2/milk2/main.c
+++ section 1/1.2/milk2/main.c
@@ -4,46 +4,55 @@ TASK:milk2
 LANG:C
 */
 #include <stdio.h>
-int write(int *t,int start,int end);
-int find(int *t,int o,int M);
 int main()
 {
-    int t[1000050]={0};
-    int n,i,start,end,M=0;
-    FILE *fin = fopen("milk2.in","r");
+    FILE *fin  = fopen("milk2.in","r");
     FILE *fout = fopen("milk2.out","w");
+    int i,j,tmp[2];
+    int n,m;
+    int l,cl;
+    int start[5000],end[5000],fs[5000],fe[5000];
     fscanf(fin,"%d",&n);
     for(i=0;i<n;i++)
+        fscanf(fin,"%d %d\n",&start[i],&end[i]);
+    for(i=0;i<n;i++)
     {
-        fscanf(fin,"%d %d",&start,&end);
-        write(t,start,end);
-        if(end>M)
-            M=end;
-    }
-    fprintf(fout,"%d %d\n",find(t,1,M)-1,find(t,0,M)+1);
-    return 0;
-}
-int write(int *t,int start,int end)
-{
-    int i;
-    for(i=start;i<=end;i++)
-        t[i]=1;
-    return 0;
-}
-int find(int *t,int o,int M)
-{   
-    int i,l=0,tmp=0;
-    for(i=1;i<M;i++)
-    {
-        if(t[i]==o)
+        for(j=n-1;j>i;j--)
         {
-            tmp++;
-            if(t[i+1]==(1-o)&&tmp>l)
+            if(start[j]<start[j-1])
             {
-                l=tmp;
-                tmp=0;
+                tmp[1]=start[j];
+                tmp[0]=end[j];
+                start[j]=start[j-1];
+                end[j]=end[j-1];
+                start[j-1]=tmp[1];
+                end[j-1]=tmp[0];
             }
         }
     }
-    return l;
+    fs[0]=start[0];
+    fe[0]=end[0];
+    m=0;
+    for(i=1;i<n;i++)
+    {
+        if(start[i] > fe[m])
+        {
+            fs[m+1]=start[i];
+            fe[m+1]=end[i];
+            m=m+1;
+        }
+        else if(end[i] > fe[m])
+            fe[m]=end[i];
+    }
+    l=fe[0]-fs[0];
+    cl=0;
+    for(i=1;i<=m;i++)
+    {
+        if((fe[i]-fs[i])>l)
+            l=fe[i]-fs[i];
+        if((fs[i]-fe[i-1])>cl)
+            cl=fs[i]-fe[i-1];
+    }
+    fprintf(fout,"%d %d\n",l,cl);
+    return 0;
 }
